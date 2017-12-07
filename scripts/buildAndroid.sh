31fed3008ef0ee7b762929a47b28e157a8cc5a17
Kevin Groat <apps@kevingroat.io>
2017-12-07 13:40:20 -0500

    now builds JS during CircleCI build

--- scripts/buildAndroid.sh
+++ scripts/buildAndroid.sh
@@ -1,8 +1,6 @@
 
 export TERM=${TERM:-xterm}
 
-unzip -P "$PROTECTED_PASS" protected.zip
-
 echo "Building Android Local App"
 NODE_ENV=development cordova build android -- --keystore="./build.keystore" --storePassword="$STORE_PASSWORD" --alias="voa-zh" --password="$ALIAS_PASSWORD" --target local
 mv platforms/android/build/outputs/apk/debug/android-debug.apk platforms/android/build/outputs/apk/debug/android-local.apk
