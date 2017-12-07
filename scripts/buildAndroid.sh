cb41a7269fdcd43cc2d3a40ff65769741edd4fae
Kevin Groat <apps@kevingroat.io>
2017-12-07 13:31:25 -0500

    Fixing artifact storage

--- scripts/buildAndroid.sh
+++ scripts/buildAndroid.sh
@@ -5,7 +5,7 @@ unzip -P "$PROTECTED_PASS" protected.zip
 
 echo "Building Android Local App"
 NODE_ENV=development cordova build android -- --keystore="./build.keystore" --storePassword="$STORE_PASSWORD" --alias="voa-zh" --password="$ALIAS_PASSWORD" --target local
-mv platforms/android/build/outputs/apk/android-debug.apk platforms/android/build/outputs/apk/android-local.apk
+mv platforms/android/build/outputs/apk/debug/android-debug.apk platforms/android/build/outputs/apk/debug/android-local.apk
 
 echo "Building Android Beta App"
 NODE_ENV=test cordova build android -- --keystore="./build.keystore" --storePassword="$STORE_PASSWORD" --alias="voa-zh" --password="$ALIAS_PASSWORD" --target beta
