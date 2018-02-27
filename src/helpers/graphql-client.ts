8b3a315a77d749fc24305ee3eefe5096a8b6f4ca
Kevin Groat <apps@kevingroat.io>
2018-02-27 12:53:48 -0500

    Updated graphql endpoint

--- src/helpers/graphql-client.ts
+++ src/helpers/graphql-client.ts
@@ -2,11 +2,9 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// https://7kpr7ovy5d.execute-api.us-east-1.amazonaws.com/prod/graphql
-
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://7kpr7ovy5d.execute-api.us-east-1.amazonaws.com/prod/graphql',
+    uri: 'https://dev.voamobileendpoints.com/server/graphql',
   }),
   cache: new InMemoryCache(),
 })
