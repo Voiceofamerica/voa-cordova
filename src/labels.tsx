3c113ddd8a9733c2ea26e82b7bae4ee0fd86e787
Kevin Groat <apps@kevingroat.io>
2018-04-10 11:22:02 -0400

    Fixed default feedback email subject

--- src/labels.tsx
+++ src/labels.tsx
@@ -135,7 +135,7 @@ export const settingsLabels = {
   sendFeedback: '发送反馈',
   aboutVoa: '美国之音(VOA)向国际受众发送准确、平衡和全面的新闻与信息。美国之音创建于1942年，用电台向封闭和被战火蹂躏的地区播送新闻。目前美国之音已发展成为一个多媒体新闻部门，通过网络、移动设备和社交媒体，以40多种语言为人们服务。',
   feedbackEmail: 'zhapp@voanews.com',
-  feedbackSubject: '美国之声',
+  feedbackSubject: '美国之音',
   feedbackBody: '',
   shareMessage: '',
 }
