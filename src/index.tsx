0242f740d97a3c2fc921768a56ebfae993639791
Kevin Groat <apps@kevingroat.io>
2018-04-11 16:33:24 -0400

    preliminary psiphon updates

--- src/index.tsx
+++ src/index.tsx
@@ -3,61 +3,39 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
-import * as moment from 'moment'
-
 import { momentLocale } from 'labels'
+
+import * as moment from 'moment'
 
 import './globalStyle.scss'
 
 import App from './containers/App'
-import { start } from 'helpers/psiphon'
 
 const rootElement = document.getElementById('app')
 
 moment.locale(momentLocale)
 
-start().then(() => {
-  // const afsm = (window as any).AndroidFullScreen
-  // afsm && afsm.immersiveMode()
-
-  let render = (Component, cb?) => {
+let render = (Component, cb?) => {
+  ReactDOM.render(
+    <Component />,
+    rootElement,
+    cb,
+  )
+}
+if (module.hot) {
+  render = Component => {
     ReactDOM.render(
-      <Component />,
+      <AppContainer>
+        <Component />
+      </AppContainer>,
       rootElement,
-      cb,
     )
   }
-  if (module.hot) {
-    render = (Component, cb?) => {
-      ReactDOM.render(
-        <AppContainer>
-          <Component />
-        </AppContainer>,
-        rootElement,
-        cb,
-      )
-    }
 
-    module.hot.accept('./containers/App', () => {
-      const NextApp = require('./containers/App').default
-      render(NextApp)
-    })
-  }
-
-  render(App, () => {
-    setTimeout(() => {
-      const splash = (navigator as any).splashscreen
-      if (splash) {
-        splash.hide()
-      }
-    }, 3000)
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App').default
+    render(NextApp)
   })
-}).catch(err => {
-  console.error(err)
-})
+}
 
-document.addEventListener(
-  'backbutton',
-  (ev) => { ev.preventDefault() },
-  false,
-)
+render(App)
